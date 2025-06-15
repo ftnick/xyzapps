8c502b156cded2e76a584662e3af9f3685675cca
nick <nicksdistrict@gmail.com>
2025-06-15 16:05:32 +1000

    Delete 3 directory

--- 3/config.py
+++
@@ -1,8 +0,0 @@
-PREFIX = "?"
-OWNERSHIP_PREFIX = ">?"
-
-VERSION = "0.0D"
-
-ACTIVITY = "listening"
-
-INVITE_LINK = "placeholder"  # "https://discord.com/api/oauth2/authorize?client_id=0&permissions=8&scope=bot"
